48f1129c488d7c766e97edad30fe9b6d89138a3b
Fabien Chevaucher <fabien@dakis.com>
2015-05-13 19:04:07 -0400

    Display an icon for membership validity

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,9 @@
 module MembersHelper
+  def membership_icon membership
+    if membership
+      content_tag(:i, "", class: "fa fa-check")
+    else
+      content_tag(:i, "", class: "fa fa-times")
+    end
+  end
 end
